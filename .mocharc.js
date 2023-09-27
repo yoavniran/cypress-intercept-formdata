a3d529edef5c1b83456fd3cc8fc2e3bebe073ed3
yoavniran <yoavniran@gmail.com>
2023-09-27 15:44:26 +0300

    chore: test gh flow

--- .mocharc.js
+++ .mocharc.js
@@ -2,4 +2,8 @@ module.exports = {
 	require: "@babel/register",
 	file: ["test-setup.js"],
 	spec: ["src/**/*.test.js"],
+	"reporter": "json",
+	"reporterOptions": [
+		"output=./reports/mocha.json"
+	]
 };
